a77d46419db4ab2c2e3ef468c37e81d5d07ea3e7
Abhilash Asokan <abhilash.asokan@logicline.de>
2019-08-03 21:19:55 +0530

    uni test

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,32 @@
+import { TestBed } from '@angular/core/testing';
+import { CalculatorService } from './calculator.service';
+import { LoggerService } from './logger.service';
+
+describe('Calculator service', () => {
+  let calculatorService: CalculatorService, loggerSpy: any;
+
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: loggerSpy }
+      ]
+    });
+
+    calculatorService = TestBed.get(CalculatorService);
+  });
+
+  it('should add two numbers', () => {
+    const result = calculatorService.add(2, 2);
+    expect(result).toBe(4);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+  });
+
+  it('should subtract two numbers', () => {
+    const result = calculatorService.subtract(2, 2);
+    expect(result).toBe(0);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+  });
+});
